1436b9f52253223b817828f34d0fb7414f628176
Patrick Huber <182398+patrickhuber@users.noreply.github.com>
2022-11-09 22:42:01 -0500

    adds documentation. deprecates SetLevel. adds log level methods

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # go-log
 go logging abstration library
+
+## getting started
+
+```bash
+go get github.com/patrickhuber/go-log
+```
+
+## usage
+
+```golang
+import (
+    "github.com/patrickhuber/go-log"
+)
+
+func main(){
+  logger := log.Default(log.WithLevel(log.Debug))
+  logger.Trace("trace")
+  logger.Debug("debug")
+  logger.Info("info")
+  logger.Warn("warn")
+  logger.Error("error")
+}
+```
+
+```bash
+debug
+info
+warn
+error
+```
--- logger.go
+++ logger.go
@@ -6,18 +6,27 @@ import (
 
 // Logger defines a logging interface
 type Logger interface {
-	Printf(format string, v ...interface{})
-	Print(v ...interface{})
-	Println(v ...interface{})
-	Fatalf(format string, v ...interface{})
-	Fatal(v ...interface{})
-	Fatalln(v ...interface{})
-	Debug(v ...interface{})
-	Debugf(format string, v ...interface{})
-	Debugln(v ...interface{})
-	Trace(v ...interface{})
-	Tracef(format string, v ...interface{})
-	Traceln(v ...interface{})
+	Printf(format string, v ...any)
+	Print(v ...any)
+	Println(v ...any)
+	Fatalf(format string, v ...any)
+	Fatal(v ...any)
+	Fatalln(v ...any)
+	Error(v ...any)
+	Errorf(format string, v ...any)
+	Errorln(v ...any)
+	Warn(v ...any)
+	Warnf(format string, v ...any)
+	Warnln(v ...any)
+	Info(v ...any)
+	Infof(format string, v ...any)
+	Infoln(v ...any)
+	Debug(v ...any)
+	Debugf(format string, v ...any)
+	Debugln(v ...any)
+	Trace(v ...any)
+	Tracef(format string, v ...any)
+	Traceln(v ...any)
 	Level() Level
 }
 
@@ -42,42 +51,105 @@ func shouldLog(level Level, target Level) bool {
 	return level >= target
 }
 
-func (l *logger) Debug(v ...interface{}) {
+func (l *logger) Error(v ...any) {
+	if !shouldLog(l.level, ErrorLevel) {
+		return
+	}
+	l.Print(v...)
+}
+
+func (l *logger) Errorf(format string, v ...any) {
+	if !shouldLog(l.level, ErrorLevel) {
+		return
+	}
+	l.Printf(format, v...)
+}
+
+func (l *logger) Errorln(v ...any) {
+	if !shouldLog(l.level, ErrorLevel) {
+		return
+	}
+	l.Println(v...)
+}
+
+func (l *logger) Warn(v ...any) {
+	if !shouldLog(l.level, WarningLevel) {
+		return
+	}
+	l.Print(v...)
+}
+
+func (l *logger) Warnf(format string, v ...any) {
+	if !shouldLog(l.level, WarningLevel) {
+		return
+	}
+	l.Printf(format, v...)
+}
+
+func (l *logger) Warnln(v ...any) {
+	if !shouldLog(l.level, WarningLevel) {
+		return
+	}
+	l.Println(v...)
+}
+
+func (l *logger) Info(v ...any) {
+	if !shouldLog(l.level, InfoLevel) {
+		return
+	}
+	l.Print(v...)
+}
+
+func (l *logger) Infof(format string, v ...any) {
+	if !shouldLog(l.level, InfoLevel) {
+		return
+	}
+	l.Printf(format, v...)
+}
+
+func (l *logger) Infoln(v ...any) {
+	if !shouldLog(l.level, InfoLevel) {
+		return
+	}
+	l.Println(v...)
+}
+
+func (l *logger) Debug(v ...any) {
 	if !shouldLog(l.level, DebugLevel) {
 		return
 	}
 	l.Print(v...)
 }
 
-func (l *logger) Debugf(format string, v ...interface{}) {
+func (l *logger) Debugf(format string, v ...any) {
 	if !shouldLog(l.level, DebugLevel) {
 		return
 	}
 	l.Printf(format, v...)
 }
 
-func (l *logger) Debugln(v ...interface{}) {
+func (l *logger) Debugln(v ...any) {
 	if !shouldLog(l.level, DebugLevel) {
 		return
 	}
 	l.Println(v...)
 }
 
-func (l *logger) Trace(v ...interface{}) {
+func (l *logger) Trace(v ...any) {
 	if !shouldLog(l.level, TraceLevel) {
 		return
 	}
 	l.Print(v...)
 }
 
-func (l *logger) Tracef(format string, v ...interface{}) {
+func (l *logger) Tracef(format string, v ...any) {
 	if !shouldLog(l.level, TraceLevel) {
 		return
 	}
 	l.Printf(format, v...)
 }
 
-func (l *logger) Traceln(v ...interface{}) {
+func (l *logger) Traceln(v ...any) {
 	if !shouldLog(l.level, TraceLevel) {
 		return
 	}
@@ -90,6 +162,16 @@ func (l *logger) Level() Level {
 
 type LogOption func(l *logger)
 
+// WithLevel sets the logging level
+func WithLevel(level Level) LogOption {
+	return func(l *logger) {
+		l.level = level
+	}
+}
+
+// SetLevel sets the logging level
+//
+// Depricated: use WithLevel instead
 func SetLevel(level Level) LogOption {
 	return func(l *logger) {
 		l.level = level
